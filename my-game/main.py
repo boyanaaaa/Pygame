d26347b102271a5057ee7df9d6823cad94bf3c28
boyanaaaa <125132910+boyanaaaa@users.noreply.github.com>
2026-03-27 13:15:16 +0200

    Moving is added to the player, we have groups class container that holds and manages game objects and and asteroids have been added.

--- my-game/main.py
+++ my-game/main.py
@@ -1,26 +1,46 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    
+    AsteroidField.containers = (updatable, )
+    asteroid_field = AsteroidField()
+
+
     pygame.init()
-
-    print(f"Starting Asteroids with pygame version: {pygame.__version__}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
 
         for event in pygame.event.get():
-             if event.type == pygame.QUIT:
-                 return
-        
-        screen.fill("black")
+            if event.type == pygame.QUIT:
+                return
 
+        updatable.update(dt)
+        screen.fill("black")
+        for draw in drawable:
+             draw.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000 
+
+        
+
+        
 
 if __name__ == "__main__":
     main()
